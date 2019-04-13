77c5ddad2f7e1b0a86183a8820d6be95c660b10f
Jaime de la Peña <jpenara11@usal.es>
2019-04-13 16:43:44 +0200

    FINAL sinOptimizar

--- Codigo/sinOptimizar.s
+++ Codigo/sinOptimizar.s
@@ -24,8 +24,11 @@ M: 			.float		0.0,0.0,0.0
 			.float		0.0,0.0,0.0
 			.float		0.0,0.0,0.0
 
-uno:		.byte 		1
+unoInt:		.byte 		1
 			.text
+
+cero:       .float       0.0
+unoFloat:   .float       1.0
 
             ; Codigo de las operaciones
 			.global main
@@ -53,7 +56,8 @@ main:
             lf  f16,B+28
             lf  f17,B+32 
 
-            lb  r1,uno
+            lb  r1,unoInt
+            lf  f31,cero
 
             ;A + B -> Fila 1
             addf f18, f0, f9
@@ -70,6 +74,157 @@ main:
             addf f25, f7, f16
             addf f26, f8, f17
 
+            ;Calculo primera parte determinante
+            multf f27, f18, f22
+            multf f27, f27, f26
 
+            multf f28, f19, f23
+            multf f28, f28, f24
+
+            multf f29, f21, f25
+            multf f29, f29, f20
+
+            addf f30, f27, f28
+            addf f30, f30, f29
+
+            ;Calculo segunda parte determinante
+            multf f27, f20, f22
+            multf f27, f27, f24
+
+            multf f28, f19, f21
+            multf f28, f28, f26
+
+            multf f29, f23, f25
+            multf f29, f29, f18
+
+            subf f31, f31, f27
+            subf f31, f31, f28
+            subf f31, f31, f29
+
+            ;Calculo determinante
+            addf f30, f30, f31
+
+            ;Division 1 entre determinante
+            lf   f31,unoFloat
+            divf f31, f31, f30
+
+            ;Multiplicacion A por B PRIMERA FILA
+            multf f18, f0, f9
+            multf f19, f1, f12
+            multf f20, f2, f15
+
+            multf f21, f0, f10
+            multf f22, f1, f13
+            multf f23, f2, f16
+
+            multf f24, f0, f11
+            multf f25, f1, f14
+            multf f26, f2, f17
+
+            addf f18, f18, f19
+            addf f18, f18, f20
+
+            addf f19, f21, f22
+            addf f19, f19, f23
+
+            addf f20, f24, f25
+            addf f20, f20, f26
+
+            ;Multiplicacion A por B SEGUNDA FILA
+            multf f21, f3, f9
+            multf f22, f4, f12
+            multf f23, f5, f15
+
+            multf f24, f3, f10
+            multf f25, f4, f13
+            multf f26, f5, f16
+
+            multf f27, f3, f11
+            multf f28, f4, f14
+            multf f29, f5, f17
+
+            addf f21, f21, f22
+            addf f21, f21, f23
+
+            addf f22, f24, f25
+            addf f22, f22, f26
+
+            addf f23, f27, f28
+            addf f23, f23, f29
+
+            ;Multiplicacion A por B TERCERA FILA
+            multf f24, f6, f9
+            multf f25, f7, f12
+            multf f26, f8, f15
+
+            multf f27, f6, f10
+            multf f28, f7, f13
+            multf f29, f8, f16
+
+            multf f30, f6, f11
+            multf f0, f7, f14
+            multf f1, f8, f17
+
+            addf f24, f24, f25
+            addf f24, f24, f26
+
+            addf f25, f27, f28
+            addf f25, f25, f29
+
+            addf f26, f30, f0
+            addf f26, f26, f1
+
+            ;Multiplicacion de AxB por 1/Det[A+b]
+            multf f18, f18, f31
+            multf f19, f19, f31
+            multf f20, f20, f31
+
+            multf f21, f21, f31
+            multf f22, f22, f31
+            multf f23, f23, f31
+
+            multf f24, f24, f31
+            multf f25, f25, f31
+            multf f26, f26, f31
+
+            ;Cargar Matriz C
+            lf  f0,C
+            lf  f1,C+4
+            lf  f2,C+8
+            lf  f3,C+12
+            lf  f4,C+16
+            lf  f5,C+20
+            lf  f6,C+24
+            lf  f7,C+28
+            lf  f8,C+32
+
+            ;Cargar Alpha
+            lf  f9,Alpha
+
+            ;Multiplicar C por Alpha
+            multf f0, f0, f9
+            multf f1, f1, f9
+            multf f2, f2, f9
+
+            multf f3, f3, f9
+            multf f4, f4, f9
+            multf f5, f5, f9
+
+            multf f6, f6, f9
+            multf f7, f7, f9
+            multf f8, f8, f9
+
+            ;Sumar Matrices Resultantes
+            addf f0, f0, f18
+            addf f1, f1, f19
+            addf f2, f2, f20
+
+            addf f3, f3, f21
+            addf f4, f4, f22
+            addf f5, f5, f23
+
+            addf f6, f6, f24
+            addf f7, f7, f25
+            addf f8, f8, f26
 
 end:		trap 0
