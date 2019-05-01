c1a18e8fe07be18e306990f01a8f5ea8c2c1fe94
Jaime de la Peña <jpenara11@usal.es>
2019-05-01 10:16:23 +0200

    Def = 285, Sin = 389

--- Codigo/conOptimizacionDef_BU294.s
+++ Codigo/conOptimizacionDef_BU294.s
@@ -39,11 +39,10 @@ main:
             lf  f13,B+16
             lf  f8,A+32
             addf f22, f4, f13; a22
-						multf f29, f4, f13 ; EMPIEZA LAS OPERACIONES DE MULTIPLICACION A X B
             lf  f17,B+32
             lf  f5,A+20
-						lf  f14,B+20
-            addf f26, f8, f17 ; a33     
+            addf f26, f8, f17 ; a33
+            lf  f14,B+20
             lf  f7,A+28
             addf f23, f5, f14 ; a23
             multf f27, f22, f26 ; a22 x a33
@@ -51,22 +50,21 @@ main:
             lf  f16,B+28
             lf  f12,B+12
             addf f25, f7, f16 ; a32
+            lf  f6,A+24
             lf  f15,B+24
             multf f28, f23, f25 ; a23 x a32
-					  lf  f6,A+24
             addf f21, f3, f12 ; a21
-            lf  f0,A   
+            lf  f0,A
+            lf  f9,B
             multf f30, f21, f26 ; a21 x a33
-						lf  f9,B
             addf f24, f6, f15 ; a31
             lf  f1,A+4
-						multf f18, f0, f9 ; EMPIEZA LAS OPERACIONES DE MULTIPLICACION A X B
             lf  f10,B+4
-            subf f27, f27, f28 ; a22 x a33 - a23 x a32
+            subf f29, f27, f28 ; a22 x a33 - a23 x a32
             multf f31, f23, f24 ; a23 x a31
             addf f26, f0, f9 ; a11
             lf  f2,A+8
-            multf f26, f26, f27 ; PRIMER MENOR
+            multf f26, f26, f29 ; PRIMER MENOR
             lf  f11,B+8
             
             addf f19, f1, f10 ; a12
@@ -87,19 +85,19 @@ main:
             lf f30, cero ; Cargar el 0
             lf f31, unoFloat; Cargar el 1
 
-            
-						multf f19, f1, f12 ; EMPIEZA LAS OPERACIONES DE MULTIPLICACION A X B
+            multf f18, f0, f9 ; EMPIEZA LAS OPERACIONES DE MULTIPLICACION A X B
+
             addf f26, f26, f20 ; (PRIMER MENOR - SEGUNDO MENOR) + TERCER MENOR
 
 
             ;Division 1 entre determinante
 
-            multf f22, f1, f13
 
             eqf f26, f30 ; Comprobamos si el determinante es 0
             bfpt end ; Finalizamos el programa si el determinante es 0
             
-            
+            multf f19, f1, f12 ; EMPIEZA LAS OPERACIONES DE MULTIPLICACION A X B
+
             divf f31, f31, f26 ; Division 1 / det A + B
             
             ; OPERACIONES DE MULTIPLICACION A X B
@@ -110,7 +108,7 @@ main:
             addf f18, f18, f20
 
 
-            
+            multf f22, f1, f13
             multf f23, f2, f16
             addf f19, f21, f22
             multf f24, f0, f11
@@ -130,10 +128,10 @@ main:
             addf f21, f21, f22
 
             multf f24, f3, f10
-            
+            multf f25, f4, f13
             addf f21, f21, f23
             multf f26, f5, f16
-            addf f22, f24, f29
+            addf f22, f24, f25
             
             
             multf f29, f5, f17
